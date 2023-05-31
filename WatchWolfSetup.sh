a831d03eb5365e92bafb0b1808dcac4a24d1aa70
rogermiranda1000 <rogermirandaperez@hotmail.com>
2023-05-31 13:36:26 +0200

    Fix: WatchWolf plugin name being downloaded

--- WatchWolfSetup.sh
+++ WatchWolfSetup.sh
@@ -113,8 +113,10 @@ case "$opt" in
 		
 		# WatchWolf Server as usual-plugins
 		watchwolf_server_versions_base_path="https://watchwolf.dev/versions"
-		higher_version=`wget -q -O - "$watchwolf_server_versions_base_path" | grep -o -P '(?<=WatchWolf-)[\d.]+(?=-)' | sort --reverse --version-sort --field-separator=. | head -1` # get the current higher version
-		wget "$watchwolf_server_versions_base_path/WatchWolf-$higher_version-1.8-1.19.jar" -P "$servers_manager_path/usual-plugins"
+		web_contents=`wget -q -O - "$watchwolf_server_versions_base_path"`
+		higher_version=`echo "$web_contents" | grep -o -P '(?<=WatchWolf-)[\d.]+(?=-)' | sort --reverse --version-sort --field-separator=. | head -1` # get the current higher version
+		higher_version_file=`echo "$web_contents" | grep -o -P "WatchWolf-${higher_version//./\\.}-[\d.]+-[\d.]+\.jar"`
+		wget "$watchwolf_server_versions_base_path/$higher_version_file" -P "$servers_manager_path/usual-plugins"
 
 		# ClientsManager dependencies
 		sudo docker pull nikolaik/python-nodejs
