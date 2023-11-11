03996256f15ad30f7c56991c93b4ae5a2b44eba3
rogermiranda1000 <rogermirandaperez@hotmail.com>
2023-11-11 11:30:34 +0100

    Feature: scan for components

--- api-docs.py
+++ api-docs.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+
 from javascript import require
 generate = require('bytefield-svg', 'latest')
 
-def main():
+def generateSVG(component: str, out: str):
     source = """
-(draw-column-headers)
-(draw-box "Address" {:span 4})
-(draw-box "Size" {:span 2})
-(draw-box 0 {:span 2})
-(draw-gap "Payload")
-(draw-bottom)
-    """
+    (draw-column-headers)
+    (draw-box "Address" {:span 4})
+    (draw-box "Size" {:span 2})
+    (draw-box 0 {:span 2})
+    (draw-gap "Payload")
+    (draw-bottom)
+        """
     diagram = generate(source)
-    print(diagram)
+
+    with open(out, "w") as f:
+        f.write(diagram)
+
+def main():
+    components_path = './API/definitions'
+    components = [ os.path.join(components_path, f) for f in os.listdir(components_path) if f.endswith('.json') ]
+    
+    for component in components:
+        svgName = component[:component.rfind('.')] + '.svg'
+        generateSVG(component, svgName)
 
 if __name__ == '__main__':
     main()
